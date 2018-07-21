0e9b01eeff67451285eedc7820e0254da37c9867
Tahnee Downie <tahneedownie@yahoo.com>
2018-07-21 14:31:17 +0100

    Created basic structure of class files.

--- models/guest.rb
+++ models/guest.rb
@@ -1,1 +1,19 @@
 # GUEST CLASS FILE
+
+require_relative('../db/sql_runner')
+
+class Guest
+
+  attr_reader :id, :title, :first_name, :surname, :address, :email
+
+  attr_writer :title, :first_name, :surname, :address, :email
+
+
+  def initialize(options)
+    @id = options['id'].to_i if options['id']
+    @title = options['title']
+    @first_name = options['first_name']
+    @surname = options['surname']
+    @address = options['address']
+    @email = options['email']
+  end
--- models/room.rb
+++ models/room.rb
@@ -1,1 +1,12 @@
 # ROOM CLASS FILE
+
+require_relative('../db/sql_runner')
+
+class Room
+
+  attr_reader :id, :room_name
+
+  def initialize(options)
+    @id = options['id'].to_i if options['id']
+    @room_name = options['room_name']
+  end
